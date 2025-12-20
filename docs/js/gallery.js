5ddc9cacb5d7125554070f4336917e2daf2990b0
Viacheslav Pihida <vyacheslav.pigida@gmail.com>
2025-12-20 10:03:46 +0000

    12: Dynamic setting of preview pane.

--- docs/js/gallery.js
+++ docs/js/gallery.js
@@ -1,6 +1,44 @@
 //  Oh, I will find a way to make a pseudodynamic page.
 var count = 51;
 var index = count-1;
+var preview_capacity = 8;
+var preview_offset = 0;
+
+function update_image(id, index){
+    console.log(id, index);
+        document.getElementById(id).src = "/images/gallery/"+index+".png";
+}
+
+function create(element, id, innerHTML){
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement(element);
+    temp.id = id;
+    temp.innerHtml = innerHTML;
+    frag.appendChild(temp);
+    return frag;
+}
+
+function update_previews(){
+    pane = document.getElementById("right-pane");
+    while(pane.firstChild){
+        pane.removeChild(pane.lastChild);
+    }
+    pane.appendChild(create("button", "bt-preview-up", ""));
+    document.getElementById("bt-preview-up").innerHTML = "&#9650;";
+
+    for(let i = 0; i<preview_capacity; i++){
+        pane.appendChild(create("button", "bt-preview-"+i, "")); 
+        button = document.getElementById("bt-preview-"+i);
+        button.classList.add("preview-button");
+        image = create("img", "preview-img-"+i, "");
+        button.appendChild(image);
+        text = create("a", "preview-a-"+i, "");
+        button.appendChild(text);
+        update_image("preview-img-"+i, i);
+    }
+    pane.appendChild(create("button", "bt-preview-down", ""));
+    document.getElementById("bt-preview-down").innerHTML = "&#9660;";
+}
 
 $(document).ready(function(){
     $("#bt-pic-bg").click(function(){
@@ -20,7 +58,7 @@ $(document).ready(function(){
                 colorName = "#202020";
             }
             $("#picture-pane").css("background-color", colorName);
-            return "<a>Background: <a style=\"color:"+colorName+";\">"+text+"</a></a>";
+            return "<a>Background: "+text+"</a>";
         });
     });
     $("#bt-prev").click(function(){
@@ -38,11 +76,12 @@ $(document).ready(function(){
             .then(data => { 
                 document.getElementById("description-text").innerHTML = data;
             });
-        document.getElementById("picture").src ="/images/gallery/"+index+".png";
+        update_image("picture", index);
         document.getElementById("picture-link").href ="https://github.com/Revenge-of-Shadow/Revenge-of-Shadow.github.io/tree/main/docs/images/gallery/"+index+".png";
     }
+
     document.location.hash=index;
     window.onhashchange();
-
+    update_previews();
 
 });
